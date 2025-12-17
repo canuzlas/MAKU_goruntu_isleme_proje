3e6b35b0935b4c8c15c9f195b9f7a3a385014336
canuzlas <79049730+canuzlas@users.noreply.github.com>
2025-12-17 17:41:47 +0300

    .

---
+++ mesafe_olcer.py
@@ -0,0 +1,85 @@
+import cv2
+import mediapipe as mp
+import pygame
+import os
+import sys
+
+UYARI_MESAFESI = 30       # 30 cm'den yakınsa uyarı ver
+GERCEK_GOZ_ARALIGI = 6.3  # İnsanın iki göz bebeği arası ortalama mesafe (cm)
+ODAK_UZAKLIGI = 650       # w:640 h480 için kalibre edilmiş odak uzaklığı (focal length) (reddit)
+SES_DOSYASI = os.path.join(os.path.dirname(__file__), "assets/uzaklas.mp3")
+
+pygame.mixer.init()
+try:
+    uyari_sesi = pygame.mixer.Sound(SES_DOSYASI)
+except FileNotFoundError:
+    print(f"HATA: '{SES_DOSYASI}' bulunamadı!")
+    sys.exit()
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(
+    refine_landmarks=True 
+)
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+print("Program çalışıyor... Çıkmak için 'q' tuşuna basın.")
+
+while True:
+    basarili, kare = cap.read()
+    if not basarili:
+        break
+
+    kare = cv2.flip(kare, 1)
+    
+    rgb_kare = cv2.cvtColor(kare, cv2.COLOR_BGR2RGB)
+    
+    sonuclar = face_mesh.process(rgb_kare)
+    
+    if sonuclar.multi_face_landmarks:
+        for yuz_noktalari in sonuclar.multi_face_landmarks:
+            
+
+            sol_iris = yuz_noktalari.landmark[468]
+            sag_iris = yuz_noktalari.landmark[473]
+
+            h, w, _ = kare.shape
+            x1, y1 = int(sol_iris.x * w), int(sol_iris.y * h)
+            x2, y2 = int(sag_iris.x * w), int(sag_iris.y * h)
+
+            piksel_mesafe = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+            
+            mesafe_cm = (ODAK_UZAKLIGI * GERCEK_GOZ_ARALIGI) / piksel_mesafe
+
+            if mesafe_cm < UYARI_MESAFESI:
+                durum = "COK YAKIN!"
+                renk = (0, 0, 255) # Kırmızı
+                
+                if not pygame.mixer.get_busy():
+                    uyari_sesi.play()
+            else:
+                durum = "GUVENLI"
+                renk = (0, 255, 0) # Yeşil
+
+        
+            cv2.circle(kare, (x1, y1), 3, (0, 255, 255), -1)
+            cv2.circle(kare, (x2, y2), 3, (0, 255, 255), -1)
+            
+            cv2.putText(kare, f"Mesafe: {int(mesafe_cm)} cm", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, renk, 2)
+            cv2.putText(kare, f"Durum: {durum}", (30, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.8, renk, 2)
+
+    cv2.imshow('Mesafe Olcer', kare)
+
+    # 'q' tuşuna basılırsa döngüden çık
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+# çalıştırmak için terminalde:
+# 1) pip install -r requirements.txt
+# 2) .venv/bin/python mesafe_olcer.py
